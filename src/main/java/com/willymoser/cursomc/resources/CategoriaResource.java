2fddc624c6ee1798c271132833755b5332374949
Willy Moser <willymoser01@gmail.com>
2021-03-28 12:39:24 -0300

    Criando entidade Categoria, adicionando Banco de dados H2 e criação automática da base de dados

---
+++ src/main/java/com/willymoser/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,26 @@
+package com.willymoser.cursomc.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.willymoser.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");		
+		List<Categoria> lista = Arrays.asList(cat1, cat2);
+		
+		return lista;
+	}
+
+}
