05162ce6e1320968f767027317549f6cf9209680
Willy Moser <willymoser01@gmail.com>
2021-03-28 17:26:08 -0300

    Adicionando entidade Pedido e Pagamento, mapeamento OneToOne, mapeamento entre heranças, evitando problema de referencia ciclica entre Estado e Cidade

--- src/main/java/com/willymoser/cursomc/CursomcApplication.java
+++ src/main/java/com/willymoser/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.willymoser.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.willymoser.cursomc.domain.Cidade;
 import com.willymoser.cursomc.domain.Cliente;
 import com.willymoser.cursomc.domain.Endereco;
 import com.willymoser.cursomc.domain.Estado;
+import com.willymoser.cursomc.domain.Pagamento;
+import com.willymoser.cursomc.domain.PagamentoComBoleto;
+import com.willymoser.cursomc.domain.PagamentoComCartao;
+import com.willymoser.cursomc.domain.Pedido;
 import com.willymoser.cursomc.domain.Produto;
+import com.willymoser.cursomc.domain.enums.EstadoPagamento;
 import com.willymoser.cursomc.domain.enums.TipoCliente;
 import com.willymoser.cursomc.repositories.CategoriaRepository;
 import com.willymoser.cursomc.repositories.CidadeRepository;
 import com.willymoser.cursomc.repositories.ClienteRepository;
 import com.willymoser.cursomc.repositories.EnderecoRepository;
 import com.willymoser.cursomc.repositories.EstadoRepository;
+import com.willymoser.cursomc.repositories.PagamentoRepository;
+import com.willymoser.cursomc.repositories.PedidoRepository;
 import com.willymoser.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,6 +103,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:32"), cli1, e2);
+		
+		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pgto1);
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 19:32"), null);
+		ped2.setPagamento(pgto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
 		
 	}
 
---
+++ src/main/java/com/willymoser/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.willymoser.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.willymoser.cursomc.domain.Cliente;
+import com.willymoser.cursomc.repositories.ClienteRepository;
+import com.willymoser.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cliente = repo.findById(id);
+		return cliente.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo:" + Cliente.class.getName()));
+	}
+
+}
